86860507c1e8ff61ee0e898e4d45e51be98fa756
hash-walker <hamzafaraz1821@gmail.com>
2025-03-01 10:04:06 +0500

    about generics in go

---
+++ generics/parametric-constraints/main.go
@@ -0,0 +1,211 @@
+/*
+Parametric Constraints
+Your interface definitions, which can later be used as constraints, can accept type parameters as well.
+
+// The store interface represents a store that sells products.
+// It takes a type parameter P that represents the type of products the store sells.
+type store[P product] interface {
+	Sell(P)
+}
+
+type product interface {
+	Price() float64
+	Name() string
+}
+
+type book struct {
+	title  string
+	author string
+	price  float64
+}
+
+func (b book) Price() float64 {
+	return b.price
+}
+
+func (b book) Name() string {
+	return fmt.Sprintf("%s by %s", b.title, b.author)
+}
+
+type toy struct {
+	name  string
+	price float64
+}
+
+func (t toy) Price() float64 {
+	return t.price
+}
+
+func (t toy) Name() string {
+	return t.name
+}
+
+// The bookStore struct represents a store that sells books.
+type bookStore struct {
+	booksSold []book
+}
+
+// Sell adds a book to the bookStore's inventory.
+func (bs *bookStore) Sell(b book) {
+	bs.booksSold = append(bs.booksSold, b)
+}
+
+// The toyStore struct represents a store that sells toys.
+type toyStore struct {
+	toysSold []toy
+}
+
+// Sell adds a toy to the toyStore's inventory.
+func (ts *toyStore) Sell(t toy) {
+	ts.toysSold = append(ts.toysSold, t)
+}
+
+// sellProducts takes a store and a slice of products and sells
+// each product one by one.
+func sellProducts[P product](s store[P], products []P) {
+	for _, p := range products {
+		s.Sell(p)
+	}
+}
+
+func main() {
+	bs := bookStore{
+		booksSold: []book{},
+	}
+
+    // By passing in "book" as a type parameter, we can use the sellProducts function to sell books in a bookStore
+	sellProducts[book](&bs, []book{
+		{
+			title:  "The Hobbit",
+			author: "J.R.R. Tolkien",
+			price:  10.0,
+		},
+		{
+			title:  "The Lord of the Rings",
+			author: "J.R.R. Tolkien",
+			price:  20.0,
+		},
+	})
+	fmt.Println(bs.booksSold)
+
+    // We can then do the same for toys
+	ts := toyStore{
+		toysSold: []toy{},
+	}
+	sellProducts[toy](&ts, []toy{
+		{
+			name:  "Lego",
+			price: 10.0,
+		},
+		{
+			name:  "Barbie",
+			price: 20.0,
+		},
+	})
+	fmt.Println(ts.toysSold)
+}
+
+Assignment
+The chief architect at Mailio has decided she wants to implement billing with generics. Specifically, she wants us to create a new biller interface. A biller is an interface that can be used to charge a customer, and it can also report its name.
+
+There are two kinds of billers:
+
+userBiller (cheaper)
+orgBiller (more expensive)
+A customer is either a user or an org. A user will be billed with a userBiller and an org with an orgBiller.
+
+Create the new biller interface. It should have 2 methods:
+
+Charge
+Name
+The good news is that the architect already wrote the userBiller and orgBiller types for us that fulfill this new biller interface. Use the definitions of those types and their methods to figure out how to write the biller interface definition.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+type biller[C customer] interface {
+	Charge(C) bill
+	Name() string 
+}
+
+// function to charge a customer using a biller interface
+
+func Charge[C customer](b biller[C], c C) bill{
+	return b.Charge(c)
+}
+
+func Name[C customer](b biller[C], c C) string {
+	return b.Name()
+}
+
+// don't edit below this line
+
+type userBiller struct {
+	Plan string
+}
+
+func (ub userBiller) Charge(u user) bill {
+	amount := 50.0
+	if ub.Plan == "pro" {
+		amount = 100.0
+	}
+	return bill{
+		Customer: u,
+		Amount:   amount,
+	}
+}
+
+func (sb userBiller) Name() string {
+	return fmt.Sprintf("%s user biller", sb.Plan)
+}
+
+type orgBiller struct {
+	Plan string
+}
+
+func (ob orgBiller) Name() string {
+	return fmt.Sprintf("%s org biller", ob.Plan)
+}
+
+func (ob orgBiller) Charge(o org) bill {
+	amount := 2000.0
+	if ob.Plan == "pro" {
+		amount = 3000.0
+	}
+	return bill{
+		Customer: o,
+		Amount:   amount,
+	}
+}
+
+type customer interface {
+	GetBillingEmail() string
+}
+
+type bill struct {
+	Customer customer
+	Amount   float64
+}
+
+type user struct {
+	UserEmail string
+}
+
+func (u user) GetBillingEmail() string {
+	return u.UserEmail
+}
+
+type org struct {
+	Admin user
+	Name  string
+}
+
+func (o org) GetBillingEmail() string {
+	return o.Admin.GetBillingEmail()
+}
+
+
